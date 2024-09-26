42801a3f5a3e162569272fdcd257d9c8fe39fb82
Christian Grimm <grimmchristian@gmx.de>
2024-09-26 21:24:51 +0200

    Solve #3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let splitText = text.split("");
+    let arrayReverse = splitText.reverse();
+    return arrayReverse.join("");
 };
 
 // Do not edit below this line

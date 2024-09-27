1916d4e43d23ae84b6f3a6d9d34ced59c3392879
Christian Grimm <grimmchristian@gmx.de>
2024-09-27 09:48:34 +0200

    Solve #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...targets) {
+    let filteredArray = list.filter((element) => {
+        return !targets.includes(element);
+    });
+    return filteredArray;
 };
 
+removeFromArray([1,2,3], 3,5);
 // Do not edit below this line
 module.exports = removeFromArray;

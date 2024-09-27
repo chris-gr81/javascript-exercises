62a2aa15e4eec218ea33c3b208d51ce9a77b7818
Christian Grimm <grimmchristian@gmx.de>
2024-09-27 10:48:55 +0200

    Solve #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    function isValid(num) {
+        return (num >= 0 && Number.isInteger(num)) ? true : false;
+    }
+
+    if (!isValid(num1) || !isValid(num2)) {
+        return "ERROR";
+    } 
+
+    let numMin = Math.min(num1, num2);
+    let numMax = Math.max(num1, num2);
+
+    let sum = 0;
+    for (numMin; numMin <= numMax; numMin++) {
+        sum += numMin;
+    }
+
+    return sum;
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;

41c6ce552786d1c35e529a05eaaf9195a29c6639
Christian Grimm <grimmchristian@gmx.de>
2024-09-30 21:10:09 +0200

    Solve #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((temp, next) => {
+    return temp + next;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((temp, next) => temp * next);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = num;
+  if (num === 0 || num === 1)
+    return 1;
+  while(num > 1) {
+    num --;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line

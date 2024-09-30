1d05dc26cbfa435088db97dca6a8cf261572cfd6
Christian Grimm <grimmchristian@gmx.de>
2024-09-30 21:40:26 +0200

    Solve #9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const alpha = [
+        "a", "b", "c", "d", "e", "f", "g",
+        "h", "i", "j", "k", "l", "m", "n",
+        "o", "p", "q", "r", "s", "t", "u",
+        "v", "w", "x", "y", "z", "0", "1",
+        "2", "3", "4", "5", "6", "7", "8",
+        "9"
+    ]
 
+    const straight = phrase
+        .toLowerCase()
+        .split("")
+        .filter((prop) => alpha.includes(prop));
+
+    const reversed = [...straight].reverse();
+    return (straight.join("") === reversed.join(""))
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;

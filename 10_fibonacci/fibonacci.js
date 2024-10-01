8511f68f05173685f0a1531fcbaa179e5b80b715
Christian Grimm <grimmchristian@gmx.de>
2024-10-01 17:56:45 +0200

    Solve #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(prop) {
+    let num = parseInt(prop);
+    if (num < 0) return "OOPS";
+    let sum = 0;
+    a = 1;
+    b = 0;
+    while (num > 0) {
+        sum = a;
+        a = a + b;
+        b = sum;
+        num--;
+    }
+    return b;
 };
 
+
+
+console.log(fibonacci(3));
 // Do not edit below this line
 module.exports = fibonacci;

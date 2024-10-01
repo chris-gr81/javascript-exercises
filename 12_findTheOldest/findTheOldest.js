1a426ca9cbf0c4330205281171d1fe8a7c3aa837
Christian Grimm <grimmchristian@gmx.de>
2024-10-01 21:36:32 +0200

    Solve #12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people
+        .map((item) => {
+            if (!item.yearOfDeath) {
+                item.yearOfDeath = new Date().getFullYear();
+            }
+            return item;
+        })
+        .sort((a, b) => {
+            firstGuy = a.yearOfDeath - a.yearOfBirth;
+            nextGuy = b.yearOfDeath - b.yearOfBirth;
+            return nextGuy - firstGuy;
+        })[0];
+    
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;

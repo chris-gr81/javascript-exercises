a50d3e3e0305e4737a06dba1c214ed72c474239e
Christian Grimm <grimmchristian@gmx.de>
2024-10-01 20:40:45 +0200

    Solve #11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bib) {
+    return bib.map((item) => item.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
